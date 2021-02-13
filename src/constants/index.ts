cce0395fae925a35d0eb1bd4239425289e69d9e4
Maor <smaorus@gmail.com>
2021-02-13 15:06:08 +0200

    adding constant ETH address and a map of network -> getherer address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,6 +5,12 @@ import { fortmatic, injected, portis, walletconnect, walletlink } from '../conne
 
 export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 
+export const GethererSwapper: { [chainId in ChainId]: string } = {
+  [ChainId.RINKEBY]: '0x523c65458B0E0Ab320f9524b9bEDDa373521FeE0'
+}
+
+export const ETH_ADDRESS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
+
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 export { PRELOADED_PROPOSALS } from './proposals'
